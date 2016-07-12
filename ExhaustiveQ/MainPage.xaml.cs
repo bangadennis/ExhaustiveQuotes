676b6c09c497f1da2ae59b4de650000df343c7cd
banga <dennisbanga@gmail.com>
2016-07-12 12:59:24 +0300

    Migration to git
    Migration to git

---
+++ ExhaustiveQ/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace ExhaustiveQ
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void category_Click_bttn(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+        }
+
+        private void Authors_Click(object sender, RoutedEventArgs e)
+        {
+
+            NavigationService.Navigate(new Uri("/stories.xaml", UriKind.Relative));
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/aboutpage.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ ExhaustiveQ/Page1.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace ExhaustiveQ
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        public void GettingDataFromPage1(object sender, RoutedEventArgs e)
+        {
+            
+        
+        }
+
+        private void categorybackbttn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void lifebttn(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Plife.xaml", UriKind.Relative));
+            
+
+        }
+
+        private void inspire_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/PivotPage1.xaml", UriKind.Relative));
+        }
+
+        private void success_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Panoramasuccess2.xaml", UriKind.Relative));
+        }
+
+        private void motivation_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pmotivational.xaml", UriKind.Relative));
+        }
+
+      
+
+       
+    }
+}
